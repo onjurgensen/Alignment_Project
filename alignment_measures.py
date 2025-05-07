f3c9ecd18a4632836a4024be4a8efa352a6b5e04
onjurgensen <ole.jurgensen@student.uva.nl>
2025-05-07 14:15:22 +0200

    enhance rsa function; add documentation for metric options

--- alignment_measures.py
+++ alignment_measures.py
@@ -111,6 +111,11 @@ def ridge_regression_cv(X_train, Y_train, X_test, Y_test, alphas=np.logspace(-8,
 
 
 def rsa(X, Y, metric='correlation', method='spearman'):
+
+    """
+    use 'correlation' or 'squared_euclidean' for metric
+    
+    """
     if metric != "cosine":
         rsa = similarity.make(f"measure/rsatoolbox/rsa-rdm={metric}-compare={method}")
     else:
